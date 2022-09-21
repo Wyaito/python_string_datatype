3f56ca2526100d1487a8f7db2894214d95133ae6
Wyatt Lynch <wyguy1121@gmail.com>
2022-09-20 21:22:09 -0400

    d

--- python_strings.py
+++ python_strings.py
@@ -8,9 +8,10 @@
 #       -set this equal to your birth year (doesn't have to be real should be less then 100 yrs ago)
 #   - current_year
 #       -set this equal to 2020
-
-
-
+my_first_name = 'Wyatt'
+my_last_name = 'Lynch'
+my_year_of_birth = '2002'
+current_year = '2022'
 
 # TODO String Indexing
 #   - Print the following items (one per line) (print using variables)
@@ -20,7 +21,8 @@
 #       - second letter of your last name (use the -index)
 #       - first two letter of your first name (use the +index)
 #       - second two letter of your last name (use the -index)
-
+print my_first_name
+print my_last_name
 
 
 
